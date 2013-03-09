8485730c5cb5169b855c1b457be6f466829a6a40
jbtule <jay+code@tuley.name>
2013-03-09 17:22:27 -0600

    standard arguments for keyczar tool

--- keyczart/dkeyczart.go
+++ keyczart/dkeyczart.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/dgryski/dkeyczar"
+	"github.com/jessevdk/go-flags"
 	"io/ioutil"
 	"os"
 	"strconv"
+	"time"
 )
 
 func Save(location string, km dkeyczar.KeyManager, crypter dkeyczar.Crypter) {
@@ -35,70 +36,73 @@ func Update(location string, km dkeyczar.KeyManager, crypter dkeyczar.Crypter) {
 
 func main() {
 
-	optLocation := flag.String("location", "", "the location of the keys")
-	optSize := flag.Int("size", 0, "the size of key to generate in bits")
-	optName := flag.String("name", "", "the name of the key")
-	optPurpose := flag.String("purpose", "", "the purpose of the key (crypt/sign)")
-	optCrypter := flag.String("crypter", "", "the key to use when dealing with encrypted keys")
-	optAsymmetric := flag.String("asymmetric", "", "the asymmetric algorithm to use (dsa/rsa)")
-	optStatus := flag.String("status", "", "the status (active/primary/inactive)")
-	optVersion := flag.Int("version", 0, "the version of the key to use")
-	optDestination := flag.String("destination", "", "the location to store the exported keys")
-
-	flag.Parse()
-
-	command := flag.Arg(0)
-
-	if command == "" {
-		fmt.Println("missing command, need: create addkey promote demote pubkey")
-		flag.Usage()
-		return
+	//value string `short:"" long:"" description:""`
+	var createOpts struct {
+		Location   string `short:"l" long:"location" description:"The location of the key set."`
+		Purpose    string `short:"o" long:"purpose"  description:"The purpose of the key set (sign|crypt)."`
+		Name       string `short:"n" long:"name" description:"The key set name."`
+		Asymmetric string `short:"a" long:"asymmetric" description:"Use asymmetric algorithm (dsa|rsa)."`
 	}
+	var addKeyOpts struct {
+		Location string `short:"l" long:"location" description:"The location of the key set."`
+		Status   string `short:"s" long:"status" description:"The status (active|primary)."`
+		Size     int    `short:"b" long:"size" description:"The key size in bits."`
+		Crypter  string `short:"c" long:"crypter" description:"The location of the crypter key set to crypt the main key set."`
+	}
+	var promoteOpts struct {
+		Location string `short:"l" long:"location" description:"The location of the key set."`
+		Version  int    `short:"v" long:"version" default:"0" description:"The key version."`
+	}
+	var demoteOpts struct {
+		Location string `short:"l" long:"location" description:"The location of the key set."`
+		Version  int    `short:"v" long:"version" default:"0" description:"The key version."`
+	}
+	var revokeOpts struct {
+		Location string `short:"l" long:"location" description:"The location of the key set."`
+		Version  int    `short:"v" long:"version" default:"0" description:"The key version."`
+	}
+	var pubKeyOpts struct {
+		Location    string `short:"l" long:"location" description:"The location of the key set."`
+		Destination string `short:"d" long:"destination" description:"The destination location of the operation."`
+		Crypter     string `short:"c" long:"crypter" description:"The location of the crypter key set to crypt the main key set."`
+	}
+	var useKeyOpts struct {
+		Format       string `long:"format" description:"Output usage for key (crypt|sign|sign-timeout|sign-vanilla|sign-attached|crypt-session|crypt-signedsession)."`
+		Location     string `short:"l" long:"location" description:"The location of the key set."`
+		Location2    string `long:"location2" description:"The location of the 2nd key set."`
+		Destination  string `short:"d" long:"destination" description:"The destination location of the operation."`
+		Destination2 string `long:"destination2" description:"The second destination location of the operation."`
+		Crypter      string `short:"c" long:"crypter" description:"The location of the crypter key set to crypt the main key set."`
+		Crypter2     string `short:"c" long:"crypter2" description:"The location of the crypter key set to crypt the 2nd key set."`
+	}
+
+	parser := flags.NewNamedParser("dkeyczart", flags.Default)
+	parser.AddCommand("create", "Create a new key set.", "Create a new key set.", &createOpts)
+	parser.AddCommand("addkey", "Add a new key to an existing key set.", "Add a new key to an existing key set.", &addKeyOpts)
+	parser.AddCommand("promote", "Promote a given key version from the key set.", "Promote a given key version from the key set.", &promoteOpts)
+	parser.AddCommand("demote", "Demote a given key version from the key set.", "Demote a given key version from the key set.", &demoteOpts)
+	parser.AddCommand("revoke", "Revoke a given key version from the key set.", "Revoke a given key version from the key set.", &revokeOpts)
+	parser.AddCommand("pubkey", "Extracts public keys to a new key set.", "Extracts public keys to a new key set.", &pubKeyOpts)
+	parser.AddCommand("usekey", "Uses keyset to encrypt or sign a message.", "Uses keyset to encrypt or sign a message.", &useKeyOpts)
+
+	args, err := parser.Parse()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
 
 	var crypter dkeyczar.Crypter
 
-	if *optCrypter != "" {
-		fmt.Println("using crypter:", *optCrypter)
-		r := dkeyczar.NewFileReader(*optCrypter)
-		var err error
-		crypter, err = dkeyczar.NewCrypter(r)
-		if err != nil {
-			fmt.Println("failed to load crypter:", err)
-			return
-		}
-	}
-
 	km := dkeyczar.NewKeyManager()
-
-	if command != "create" {
-
-		if *optLocation == "" {
-			fmt.Println("missing required --location argument")
-			return
-		}
-
-		lr := dkeyczar.NewFileReader(*optLocation)
-
-		if crypter != nil {
-			fmt.Println("decrypting keys..")
-			lr = dkeyczar.NewEncryptedReader(lr, crypter)
-		}
-
-		err := km.Load(lr)
-		if err != nil {
-			fmt.Println("failed to load key:", err)
-			return
-		}
-	}
 
 	switch command {
 
 	case "create":
-		// make sure location doesn't exist
 
 		keypurpose := dkeyczar.P_TEST
 
-		switch *optPurpose {
+		switch createOpts.Purpose {
 		case "crypt":
 			keypurpose = dkeyczar.P_DECRYPT_AND_ENCRYPT
 		case "sign":
@@ -107,54 +111,64 @@ func main() {
 			fmt.Println("must provide a purpose with --purpose")
 			return
 		default:
-			fmt.Println("unknown cryptographic purpose:", *optPurpose)
+			fmt.Println("unknown cryptographic purpose:", createOpts.Purpose)
 			return
 		}
 
-		if *optAsymmetric != "" && *optAsymmetric != "dsa" && *optAsymmetric != "rsa" {
-			fmt.Println("unknown asymmetric key type:", *optAsymmetric)
+		if createOpts.Asymmetric != "" && createOpts.Asymmetric != "dsa" && createOpts.Asymmetric != "rsa" {
+			fmt.Println("unknown asymmetric key type:", createOpts.Asymmetric)
 			return
 		}
 
 		keytype := dkeyczar.T_AES
 
 		switch {
-		case keypurpose == dkeyczar.P_DECRYPT_AND_ENCRYPT && *optAsymmetric == "":
+		case keypurpose == dkeyczar.P_DECRYPT_AND_ENCRYPT && createOpts.Asymmetric == "":
 			keytype = dkeyczar.T_AES
-		case keypurpose == dkeyczar.P_DECRYPT_AND_ENCRYPT && *optAsymmetric == "rsa":
+		case keypurpose == dkeyczar.P_DECRYPT_AND_ENCRYPT && createOpts.Asymmetric == "rsa":
 			keytype = dkeyczar.T_RSA_PRIV
-		case keypurpose == dkeyczar.P_SIGN_AND_VERIFY && *optAsymmetric == "":
+		case keypurpose == dkeyczar.P_SIGN_AND_VERIFY && createOpts.Asymmetric == "":
 			keytype = dkeyczar.T_HMAC_SHA1
-		case keypurpose == dkeyczar.P_SIGN_AND_VERIFY && *optAsymmetric == "rsa":
+		case keypurpose == dkeyczar.P_SIGN_AND_VERIFY && createOpts.Asymmetric == "rsa":
 			keytype = dkeyczar.T_RSA_PRIV
-		case keypurpose == dkeyczar.P_SIGN_AND_VERIFY && *optAsymmetric == "dsa":
+		case keypurpose == dkeyczar.P_SIGN_AND_VERIFY && createOpts.Asymmetric == "dsa":
 			keytype = dkeyczar.T_DSA_PRIV
 		default:
-			fmt.Println("unknown or invalid purpose/asymmetric combination:", *optPurpose, "/", *optAsymmetric)
+			fmt.Println("unknown or invalid purpose/asymmetric combination:", createOpts.Purpose, "/", createOpts.Asymmetric)
 			return
 		}
 
-		km.Create(*optName, keypurpose, keytype)
+		km.Create(createOpts.Name, keypurpose, keytype)
 
-		Save(*optLocation, km, crypter)
+		Save(createOpts.Location, km, crypter)
 
 	case "promote":
-		if *optVersion == 0 {
+		if !loadLocationReader(km, promoteOpts.Location, "") {
+			return
+		}
+		if promoteOpts.Version == 0 {
 			fmt.Println("must provide a version with --version")
 			return
 		}
-		km.Promote(*optVersion)
-		Update(*optLocation, km, crypter)
+		km.Promote(promoteOpts.Version)
+		Update(promoteOpts.Location, km, crypter)
 	case "demote":
-		if *optVersion == 0 {
+		if !loadLocationReader(km, demoteOpts.Location, "") {
+			return
+		}
+
+		if demoteOpts.Version == 0 {
 			fmt.Println("must provide a version with --version")
 			return
 		}
-		km.Demote(*optVersion)
-		Update(*optLocation, km, crypter)
+		km.Demote(demoteOpts.Version)
+		Update(demoteOpts.Location, km, crypter)
 	case "addkey":
+		if !loadLocationReader(km, addKeyOpts.Location, "") {
+			return
+		}
 		status := dkeyczar.S_ACTIVE
-		switch *optStatus {
+		switch addKeyOpts.Status {
 		case "":
 			// FIXME: really, want to do: status = (km.kz.primary == -1 ? S_PRIMARY : S_ACTIVE)
 			status = dkeyczar.S_ACTIVE
@@ -165,19 +179,151 @@ func main() {
 		case "inactive":
 			status = dkeyczar.S_INACTIVE
 		default:
-			fmt.Println("unknown status:", *optStatus)
+			fmt.Println("unknown status:", addKeyOpts.Status)
 			return
 		}
 
-		err := km.AddKey(uint(*optSize), status)
+		err := km.AddKey(uint(addKeyOpts.Size), status)
 		if err != nil {
 			fmt.Println("error adding key:", err)
 			return
 		}
-		Update(*optLocation, km, crypter)
+		Update(addKeyOpts.Location, km, crypter)
 	case "pubkey":
+		if !loadLocationReader(km, pubKeyOpts.Location, "") {
+			return
+		}
 		kpub := km.PubKeys()
-		Save(*optDestination, kpub, nil) // doesn't make sense to encrypt a public key
+		Save(pubKeyOpts.Location, kpub, nil) // doesn't make sense to encrypt a public key
+		return
+	case "usekey":
+		r := loadReader(useKeyOpts.Location, useKeyOpts.Crypter)
+		if r == nil {
+			return
+		}
+		var output string
+		var output2 string
+		if len(args) == 0 {
+			fmt.Println("must provide input")
+			return
+		}
+		input := []byte(args[0])
+		switch useKeyOpts.Format {
+		case "crypt":
+			encrypter, _ := dkeyczar.NewEncrypter(r)
+			output, _ = encrypter.Encrypt(input)
+		case "sign":
+			signer, _ := dkeyczar.NewSigner(r)
+			output, _ = signer.Sign(input)
+		case "sign-timeout":
+			if len(args) < 2 {
+				fmt.Println("must provide date")
+			}
+			t, _ := time.Parse(time.RFC3339, args[1])
+			ticks := t.Unix() * 1000
+			signer, _ := dkeyczar.NewSigner(r)
+			output, _ = signer.TimeoutSign(input, ticks)
+		case "sign-unversioned":
+			signer, _ := dkeyczar.NewSigner(r)
+			output, _ = signer.UnversionedSign(input)
+		case "sign-attached":
+			nonce := ""
+			if len(args) > 1 {
+				nonce = args[1]
+			}
+			signer, _ := dkeyczar.NewSigner(r)
+			output, _ = signer.AttachedSign(input, []byte(nonce))
+		case "crypt-session":
+			e, _ := dkeyczar.NewEncrypter(r)
+			var se dkeyczar.Crypter
+			se, output2, _ = dkeyczar.NewSessionEncrypter(e)
+			output, _ = se.Encrypt(input)
+		case "crypt-signsession":
+			r2 := loadReader(useKeyOpts.Location2, useKeyOpts.Crypter2)
+			e, _ := dkeyczar.NewEncrypter(r)
+			s, _ := dkeyczar.NewSigner(r2)
+			var se dkeyczar.SignedEncrypter
+			se, output2, _ = dkeyczar.NewSignedSessionEncrypter(e, s)
+			output, _ = se.Encrypt(input)
+		default:
+			fmt.Println("must provide a format with --format")
+			return
+		}
+		if useKeyOpts.Destination == "" {
+			fmt.Println("must provide a destination with --destination")
+			return
+		}
+		ioutil.WriteFile(useKeyOpts.Destination, []byte(output), 0600)
+		if output2 != "" {
+			if useKeyOpts.Destination2 == "" {
+				fmt.Println("must provide a Destination2 with --destination2")
+				return
+			}
+			ioutil.WriteFile(useKeyOpts.Destination2, []byte(output2), 0600)
+		}
+		return
+	}
+}
+
+func loadReader(optLocation string, optCrypter string) dkeyczar.KeyReader {
+	if optLocation == "" {
+		fmt.Println("missing required --location argument")
+		return nil
 	}
 
+	lr := dkeyczar.NewFileReader(optLocation)
+
+	var crypter dkeyczar.Crypter
+
+	if optCrypter != "" {
+		fmt.Println("using crypter:", optCrypter)
+		r := dkeyczar.NewFileReader(optCrypter)
+		var err error
+		crypter, err = dkeyczar.NewCrypter(r)
+		if err != nil {
+			fmt.Println("failed to load crypter:", err)
+			return nil
+		}
+	}
+
+	if crypter != nil {
+		fmt.Println("decrypting keys..")
+		lr = dkeyczar.NewEncryptedReader(lr, crypter)
+	}
+
+	return lr
+}
+
+func loadLocationReader(km dkeyczar.KeyManager, optLocation string, optCrypter string) bool {
+	if optLocation == "" {
+		fmt.Println("missing required --location argument")
+		return false
+	}
+
+	lr := dkeyczar.NewFileReader(optLocation)
+
+	var crypter dkeyczar.Crypter
+
+	if optCrypter != "" {
+		fmt.Println("using crypter:", optCrypter)
+		r := dkeyczar.NewFileReader(optCrypter)
+		var err error
+		crypter, err = dkeyczar.NewCrypter(r)
+		if err != nil {
+			fmt.Println("failed to load crypter:", err)
+			return false
+		}
+	}
+
+	if crypter != nil {
+		fmt.Println("decrypting keys..")
+		lr = dkeyczar.NewEncryptedReader(lr, crypter)
+	}
+
+	err := km.Load(lr)
+	if err != nil {
+		fmt.Println("failed to load key:", err)
+		return false
+	}
+	return true
 }
